9778cc85c6913d989d2582ebf0d1716e51b5c72f
Kenny Wolfenberger and Stuart Gil <stuart@>
2019-01-07 16:46:02 -0800

    updates

--- src/main.js
+++ src/main.js
@@ -5,44 +5,71 @@ import unlit2 from './img/2_off.png';
 import unlit3 from './img/3_off.png';
 import unlit4 from './img/4_off.png';
 import lit1 from './img/1_on.png';
-// import lit2 from './img/2_on.png';
-// import lit3 from './img/3_on.png';
-// import lit4 from './img/4_on.png';
+import lit2 from './img/2_on.png';
+import lit3 from './img/3_on.png';
+import lit4 from './img/4_on.png';
 
 $(document).ready(function() {
+    var sequence = [];
     $('#1').attr('src', unlit1);
     $('#2').attr('src', unlit2);
     $('#3').attr('src', unlit3);
     $('#4').attr('src', unlit4);
     $('#start-button').click(function(event) {
         event.preventDefault();
-        var sequence = generateSequence();
+        sequence = generateSequence();
+        
+        sequence.forEach(function(x){
+            setInterval(selectiveLightUp(x), 3000);
+        });
 
 
         
-        lightUp();
-        $('#test').append(sequence[0]);
-        setTimeout(lightDown, 2000);
+        function selectiveLightUp(x){
+            if (x === 1)
+            {
+                lightUp1();
+            }
+            else if (x === 2)
+            {
+                lightUp2();
+            }
+            else if (x === 3)
+            {
+                lightUp3();
+            }
+            else if (x === 4)
+            {
+                lightUp4();
+            } 
             
-        });
-        function lightUp(){
+        }
+
+        function lightUp1(){
             $('#1').attr('src', lit1);
         }
 
+        function lightUp2(){
+            $('#2').attr('src', lit2);
+        }
+
+        function lightUp3(){
+            $('#3').attr('src', lit3);
+        }
+
+        function lightUp4(){
+            $('#4').attr('src', lit4);
+        }
+
+
+
         function lightDown(){
             $('#1').attr('src', unlit1);
+            $('#2').attr('src', unlit2);
+            $('#3').attr('src', unlit3);
+            $('#4').attr('src', unlit4);
         }
 
     });
 
-
-// $(document).ready(function() {
-//   $('#ping-pong-form').submit(function(event) {
-//     event.preventDefault();
-//     var goal = $('#goal').val();
-//     var output = pingPong(goal);
-//     output.forEach(function(element) {
-//       $('#solution').append("<li>" + element + "</li>");
-//     });
-//   });
-// });
+});
